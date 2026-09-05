2255248749ae939a5992cb70b0265155d0fa30b8
Spandir24 <pandirsana@gmail.com>
2026-09-05 19:34:07 +0530

    linking react state to form

--- frontend/src/components/Transactions.jsx
+++ frontend/src/components/Transactions.jsx
@@ -1,4 +1,11 @@
+import { useState } from "react";
+
 function Transactions() {
+
+  const [title, setTitle] = useState("");
+  const [amount, setAmount] = useState("");
+  const [type, setType] = useState("income");
+
   return (
     <section className="mt-10">
       <div className="flex justify-between items-center mb-6">
@@ -8,22 +15,31 @@ function Transactions() {
           Add Transaction
         </button>
       </div>
-
       <div className="bg-white p-6 border rounded-xl shadow-sm">
         <form>
           <div>
             <label>Title</label>
-            <input type="text" placeholder=" e.g. Salary" />
+            <input
+              type="text"
+              placeholder=" e.g. Salary"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
           </div>
 
           <div>
             <label>Amount</label>
-            <input type="number" placeholder="Enter amount" />
+            <input
+              type="number" // but the value we receive (here as input) is initially treated as a string
+              placeholder=" Enter amount"
+              value={amount}
+              onChange={(e) => setAmount(e.target.value)}
+            />
           </div>
 
           <div>
             <label>Type</label>
-            <select>
+            <select value={type} onChange={(e) => setType(e.target.value)}>
               <option value="income">Income</option>
               <option value="expense">Expense</option>
             </select>
