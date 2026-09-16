e8e0db26fb4d350adaf73e22f00295022431ae85
Spandir24 <pandirsana@gmail.com>
2026-09-16 14:14:03 +0530

    Allow PATCH requests from frontend

--- backend/server.js
+++ backend/server.js
@@ -6,22 +6,13 @@ const Transaction = require("./models/Transaction");
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://smart-expense-tracker-frontend-gg4k.onrender.com",
-  );
-
-  res.header("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE,OPTIONS");
-
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(204);
-  }
-
-  next();
-});
+app.use(
+  cors({
+    origin: "https://smart-expense-tracker-frontend-gg4k.onrender.com",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type"],
+  }),
+);
 
 
 app.use(express.json());
