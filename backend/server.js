e81454153fe968ebf6f4bfd2fba3219595d46ae3
Spandir24 <pandirsana@gmail.com>
2026-09-16 14:17:14 +0530

    Restore CORS import

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const Transaction = require("./models/Transaction");
-
+const cors = require("cors");
 const app = express();
 
 app.use(
